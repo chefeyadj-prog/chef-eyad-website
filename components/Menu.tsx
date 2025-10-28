36f32203f3ee34cc182b026b245c0ee829160b61
chefeyadj-prog <chefeyad.j@gmail.com>
2025-10-28 14:40:43 +0300

    Add allergen icons for gluten, dairy, and nuts

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -18,7 +18,20 @@ interface MenuProps {
     };
   };
 }
-
+const allergenIcons: Record<Allergen, { icon: React.ReactNode; label: string }> = {
+  gluten: {
+    icon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M19.78,9.45a1,1,0,0,0-1.1-1.32,10.66,10.66,0,0,1-2.29-.19,1,1,0,0,0-1.12.83,2.44,2.44,0,0,1-4.54,0,1,1,0,0,0-1.12-.83,10.66,10.66,0,0,1-2.29.19,1,1,0,0,0-1.1,1.32,11.5,11.5,0,0,0,5.49,11.2,1,1,0,0,0,1,0A11.5,11.5,0,0,0,19.78,9.45Z"/></svg>,
+    label: 'Contains Gluten'
+  },
+  dairy: {
+    icon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M12,2A10,10,0,0,0,5.22,18.14c1.45,1.72,3.77,3.58,6.2,3.82a1,1,0,0,0,.64,0c2.43-.24,4.75-2.1,6.2-3.82A10,10,0,0,0,12,2Zm0,18a8,8,0,0,1-5.55-2.25c.55-.63,1.22-1.37,1.78-2,.6-.68,1.2-1.28,1.67-1.63A4,4,0,0,0,12,11.5a4,4,0,0,0,2.1-3.38c.47.35,1.07.95,1.67,1.63.56.63,1.23,1.37,1.78,2A8,8,0,0,1,12,20Z"/></svg>,
+    label: 'Contains Dairy'
+  },
+  nuts: {
+    icon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M19.5,10H17.21l-3.54,4.25,2.1,2.1a1,1,0,0,1,0,1.42,1,1,0,0,1-1.42,0l-2.1-2.1L8.5,19.92a1,1,0,0,1-1.42,0,1,1,0,0,1,0-1.42l2.1-2.1L4.79,10H2.5A2.5,2.5,0,0,1,0,7.5,2.5,2.5,0,0,1,2.5,5h19A2.5,2.5,0,0,1,24,7.5,2.5,2.5,0,0,1,21.5,10Z"/></svg>,
+    label: 'Contains Nuts'
+  }
+};
 const MenuItem: React.FC<MenuItemType> = ({ name, description, price, image }) => {
   if (image) {
     return (
